7287cd4236cc24e8fe83fa0a562b58b333be7b3a
Aaryan Shrivastav <145484575+aaryanshrivastav@users.noreply.github.com>
2026-01-21 21:42:50 +0530

    Update p28.r

--- p28.r
+++ p28.r
@@ -2,28 +2,44 @@
 to_do_list <- list()
 
 # Function to add tasks to the to-do list
-add_task <- function(task) {
-  to_do_list <<- c(to_do_list, list(task = task, completed = FALSE))
+add_task <- function(task)
+{
+  new_task <- list(task = task, completed = FALSE)
+  to_do_list <<- c(to_do_list, list(new_task))
 }
 
 # Function to mark tasks as completed
-mark_completed <- function(task) {
-  for (i in seq_along(to_do_list)) {
-    if (to_do_list[[i]]$task == task) {
-      to_do_list[[i]]$completed <- TRUE
+mark_completed <- function(task)
+{
+  for (i in seq_along(to_do_list))
+  {
+    if (to_do_list[[i]]$task == task)
+    {
+      to_do_list[[i]]$completed <<- TRUE
       break
     }
   }
 }
 
 # Function to view the to-do list
-view_tasks <- function() {
-  print(to_do_list)
+view_tasks <- function()
+{
+  if (length(to_do_list) == 0)
+  {
+    print("To-do list is empty.")
+    return()
+  }
+  
+  for (i in seq_along(to_do_list))
+  {
+    status <- ifelse(to_do_list[[i]]$completed, "Completed", "Pending")
+    cat(i, ". ", to_do_list[[i]]$task, " - ", status, "\n", sep = "")
+  }
 }
+
 
 # Example usage
 add_task("Finish report")
 add_task("Call client")
 mark_completed("Finish report")
 view_tasks()
-
